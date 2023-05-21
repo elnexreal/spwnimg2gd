b79d44a2f907fd245a2d828ce84083a426e5f558
Axel Pundang <lolopundang1@gmail.com>
2023-05-21 03:36:10 -0300

    added comments to the script and removed some useless things

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from colorsys import rgb_to_hsv
 from colorama import Fore
 from PIL import Image, ImageOps
 
-# HAHAHAHA LMAO SHITTY CODE (i mean it works so nice :D)
+# HAHAHAHA LMAO SHITTY CODE (i mean it works, so i don't rlly care :3)
 
 def save_pixels_to_json(image_path, output_path):
     image = Image.open(image_path)
@@ -17,19 +17,16 @@ def save_pixels_to_json(image_path, output_path):
     new_width = 50
     # DONT MODIFY THIS, THIS IS FOR KEEPING THE ASPECT RATIO
     new_height = int(height * new_width / width)
-
     image = image.resize((new_width, new_height))
 
-    # image.save("./images/converted/debug.jpg")
-
+    # iterates for every pixel
     pixels = []
-    pixel_number = 0
-
     for y in range(new_height):
         for x in range(new_width):
+            # gets current RGB for the current pixel in the iteration
             r, g, b = image.getpixel((x, y))
+            # converts the RGB color to HSV
             color = rgb_to_hsv(r, g, b)
-            # print(color)
             pixel = {
                     'posx': x,
                     'posy': y,
@@ -45,30 +42,32 @@ def save_pixels_to_json(image_path, output_path):
 
 name = input("Select the file you want to convert (you must add the file extension): ")
 
+# check if the file name provided by the user is correct
 if os.path.exists(f"./images/{name}"):
-    if "." in name:
-        if (name.endswith(".jpg")):
-            image_path = f"./images/{name}"
-        else:
-            if not os.path.exists("./images/converted"):
-                os.mkdir("./images/converted")
+    # if the image is in jpg format don't convert
+    if (name.endswith(".jpg")):
+        image_path = f"./images/{name}"
+    # if the image is rgb or another format convert it to jpg
+    else:
+        if not os.path.exists("./images/converted"):
+            os.mkdir("./images/converted")
 
-            im = Image.open(f"./images/{name}").convert("RGB")
-            im.save("./images/converted/image.jpg")
-            image_path = "./images/converted/image.jpg"
+        im = Image.open(f"./images/{name}").convert("RGB")
+        im.save("./images/converted/image.jpg")
+        image_path = "./images/converted/image.jpg"
 else:
     print(Fore.YELLOW + "File not found")
     exit()
 
-output_path = "./output.json"
-
 try:
-    save_pixels_to_json(image_path, output_path)
+    save_pixels_to_json(image_path, "./output.json")
 except Exception as err:
     print(Fore.RED + f"Error: {err}")
 else:
     print("Successfully converted to raw data, executing the spwn script... \n")
 
+# spwn script execution
 os.system("spwn build ./lib/convert.spwn --allow readfile")
 
+# remove the output json (comment this line if you want to keep the file to see it)
 os.remove('./output.json')
